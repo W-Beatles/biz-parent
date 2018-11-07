62c3597b6444bd4349b758ae785ffd202a885f89
Z-Beatles <waynechu@waynechu.cn>
2018-11-07 19:02:47 +0800

    生成mapper

---
+++ app-integration-dal/src/main/java/cn/waynechu/dal/mapper/HouseMapper.java
@@ -0,0 +1,30 @@
+package cn.waynechu.dal.mapper;
+
+import cn.waynechu.dal.entity.House;
+import cn.waynechu.dal.entity.HouseExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface HouseMapper {
+    long countByExample(HouseExample example);
+
+    int deleteByExample(HouseExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(House record);
+
+    int insertSelective(House record);
+
+    List<House> selectByExample(HouseExample example);
+
+    House selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") House record, @Param("example") HouseExample example);
+
+    int updateByExample(@Param("record") House record, @Param("example") HouseExample example);
+
+    int updateByPrimaryKeySelective(House record);
+
+    int updateByPrimaryKey(House record);
+}
