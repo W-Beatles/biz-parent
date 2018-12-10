09fec340ee3a56fcf1875df9ea1bb3f024014113
Z-Beatles <waynechu@waynechu.cn>
2018-12-10 10:08:15 +0800

    feat: 添加工具方法

--- app-common/src/main/java/cn/waynechu/common/util/CollectionUtil.java
+++ app-common/src/main/java/cn/waynechu/common/util/CollectionUtil.java
@@ -1,6 +1,8 @@
 package cn.waynechu.common.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,5 +24,22 @@ public class CollectionUtil {
 
     public static boolean isNullOrEmpty(Map<? extends Object, ? extends Object> map) {
         return map == null || map.isEmpty();
+    }
+
+    public static <T> List<List<T>> splitList(List<T> list, int len) {
+        List<List<T>> result = new ArrayList<>();
+
+        if (list == null || list.isEmpty() || len < 1) {
+            return result;
+        }
+
+        int size = list.size();
+        int count = (size + len - 1) / len;
+
+        for (int i = 0; i < count; i++) {
+            List<T> subList = list.subList(i * len, ((i + 1) * len > size ? size : (i + 1) * len));
+            result.add(subList);
+        }
+        return result;
     }
 }
