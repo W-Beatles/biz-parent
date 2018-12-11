6cd60a97000cd144a3956071fe48fedcf9634cd5
Z-Beatles <waynechu@waynechu.cn>
2018-12-11 19:53:20 +0800

    feat: 添加异常返回

--- renting-web/src/main/java/cn/waynechu/renting/web/advice/ControllerExceptionHandler.java
+++ renting-web/src/main/java/cn/waynechu/renting/web/advice/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package cn.waynechu.renting.web.advice;
 
 import cn.waynechu.common.enums.ResultEnum;
 import cn.waynechu.common.facade.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @author zhuwei
  * @date 2018/11/15 10:45
  */
+@Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
@@ -17,5 +19,11 @@ public class ControllerExceptionHandler {
     public Result missingServletRequestParameterException(MissingServletRequestParameterException e) {
         // 缺少请求参数
         return Result.error(ResultEnum.MISSING_REQUEST_PARAMETER.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public Result unknownException(Exception e) {
+        log.error("[系统异常]", e);
+        return Result.error("[系统异常] " + e.toString());
     }
 }
