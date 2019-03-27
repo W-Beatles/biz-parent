b98c4e2b1979063e0fa6a72d428b39b42cf6f4c0
Z-Beatles <waynechu@waynechu.cn>
2019-03-27 13:39:32 +0800

    feat(renting-web): 添加跨域配置

---
+++ renting-web/src/main/java/cn/waynechu/renting/web/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package cn.waynechu.renting.web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ *
+ * @author zhuwei
+ * @date 2019/3/27 13:23
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.split(","))
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                // 预检请求有效时间。默认1800
+                .maxAge(3600);
+    }
+}
