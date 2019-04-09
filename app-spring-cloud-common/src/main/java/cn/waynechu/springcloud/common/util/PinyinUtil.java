d23c7ae3ec6fa069e4472f26ca0b0bb00140b059
Z-Beatles <waynechu@waynechu.cn>
2019-04-09 17:44:29 +0800

    feat(app-spring-cloud-common): 添加汉字转拼音工具类

---
+++ app-spring-cloud-common/src/main/java/cn/waynechu/springcloud/common/util/PinyinUtil.java
@@ -0,0 +1,38 @@
+package cn.waynechu.springcloud.common.util;
+
+import com.github.promeg.pinyinhelper.Pinyin;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 汉字转拼音工具类
+ *
+ * @author zhuwei
+ * @date 2019/4/9 17:31
+ * @see "https://github.com/promeG/TinyPinyin"
+ */
+public class PinyinUtil {
+
+    /**
+     * 中文转拼音
+     *
+     * @param chineseWord 中文
+     * @return String 拼音
+     */
+    public static String toPinyin(String chineseWord) {
+
+        if (StringUtils.isBlank(chineseWord)) {
+            return "";
+        } else {
+            StringBuilder s = new StringBuilder();
+            for (char c : chineseWord.toCharArray()) {
+                s.append(Pinyin.toPinyin(c));
+            }
+            return s.toString().toLowerCase();
+        }
+    }
+
+    public static void main(String[] args) {
+        String pinyin = toPinyin("上海市");
+        System.out.println(pinyin);
+    }
+}
