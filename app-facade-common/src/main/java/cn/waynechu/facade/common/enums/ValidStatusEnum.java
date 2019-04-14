064beaec3c3e75df211e6830c9c4c933c5c1dc62
Z-Beatles <waynechu@waynechu.cn>
2019-04-14 23:48:10 +0800

    refactor(app-facade-common): 修改业务异常枚举基类; 修改logback日志格式，去除MDC打印

--- app-facade-common/src/main/java/cn/waynechu/facade/common/enums/ValidStatusEnum.java
+++ app-facade-common/src/main/java/cn/waynechu/facade/common/enums/ValidStatusEnum.java
@@ -4,18 +4,20 @@ package cn.waynechu.facade.common.enums;
  * @author zhuwei
  * @date 2018/11/6 17:24
  */
-public enum ValidStatusEnum implements AbstractEnum {
+public enum ValidStatusEnum implements BizEnum {
     /**
      * 通用 有效/无效 状态枚举
      */
-    INVALID(0, "无效"),
-    VALID(1, "有效");
+    INVALID(0, "INVALID", "无效"),
+    VALID(1, "VALID", "有效");
 
     private int code;
+    private String name;
     private String desc;
 
-    ValidStatusEnum(int code, String desc) {
+    ValidStatusEnum(int code, String name, String desc) {
         this.code = code;
+        this.name = name;
         this.desc = desc;
     }
 
@@ -25,12 +27,16 @@ public enum ValidStatusEnum implements AbstractEnum {
     }
 
     @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
     public String getDesc() {
         return this.desc;
     }
 
-    @Override
-    public AbstractEnum getByCode(int code) {
+    public ValidStatusEnum getByCode(int code) {
         for (ValidStatusEnum validStatusEnum : values()) {
             if (validStatusEnum.getCode() == code) {
                 return validStatusEnum;
--- app-spring-cloud-api-common/src/main/java/cn/waynechu/springcloud/apicommon/advice/ControllerExceptionHandler.java
+++ app-spring-cloud-api-common/src/main/java/cn/waynechu/springcloud/apicommon/advice/ControllerExceptionHandler.java
@@ -1,9 +1,11 @@
 package cn.waynechu.springcloud.apicommon.advice;
 
 import cn.waynechu.facade.common.Result;
-import cn.waynechu.facade.common.enums.CommonResultEnum;
+import cn.waynechu.facade.common.enums.BizErrorCodeEnum;
 import cn.waynechu.facade.common.exception.BizException;
+import cn.waynechu.springcloud.apicommon.mdc.MDCFilter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -24,7 +26,7 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public Result httpMessageNotReadableException(HttpMessageNotReadableException e) {
         log.info("请求参数格式不正确: {}", e.getMessage(), e);
-        return Result.error(CommonResultEnum.ARGUMENT_IS_INCORRECT);
+        return Result.error(BizErrorCodeEnum.ARGUMENT_IS_INCORRECT);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -32,28 +34,28 @@ public class ControllerExceptionHandler {
         log.info("请求参数校验不合法: {}", e.getMessage(), e);
         BindingResult bindingResult = e.getBindingResult();
         if (bindingResult == null || bindingResult.getFieldError() == null) {
-            return Result.error(CommonResultEnum.ARGUMENT_NOT_VALID);
+            return Result.error(BizErrorCodeEnum.ARGUMENT_NOT_VALID);
         }
-        return Result.error(CommonResultEnum.ARGUMENT_NOT_VALID.getCode(), bindingResult.getFieldError().getDefaultMessage());
+        return Result.error(BizErrorCodeEnum.ARGUMENT_NOT_VALID.getCode(), bindingResult.getFieldError().getDefaultMessage());
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public Result missingServletRequestParameterException(MissingServletRequestParameterException e) {
         log.info("缺少请求参数: ({}) {}", e.getParameterType(), e.getParameterName(), e);
-        return Result.error(CommonResultEnum.MISSING_REQUEST_PARAM.getCode(),
-                CommonResultEnum.MISSING_REQUEST_PARAM.getDesc()
+        return Result.error(BizErrorCodeEnum.MISSING_REQUEST_PARAM.getCode(),
+                BizErrorCodeEnum.MISSING_REQUEST_PARAM.getDesc()
                         + ": (" + e.getParameterType() + ") " + e.getParameterName());
     }
 
     @ExceptionHandler(BizException.class)
     public Result bizException(BizException e) {
         log.info("[业务异常] {}", e.getErrorMessage(), e);
-        return Result.error(e.getErrorCode(), e.getErrorMessage());
+        return Result.error(e.getErrorCode(), e.getErrorMessage(), MDC.get(MDCFilter.REQ_KEY));
     }
 
     @ExceptionHandler(Exception.class)
     public Result unknownException(Exception e) {
         log.error("[系统异常] ", e);
-        return Result.error();
+        return Result.error(BizErrorCodeEnum.SYSTEM_ERROR, MDC.get(MDCFilter.REQ_KEY));
     }
 }
