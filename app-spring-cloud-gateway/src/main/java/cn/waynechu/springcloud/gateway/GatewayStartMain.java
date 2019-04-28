9f9453a07c26a248c3bc44db6bfde739a44c2477
Z-Beatles <waynechu@waynechu.cn>
2019-04-28 16:26:52 +0800

    feat(app-spring-cloud-gateway): 添加SpringCloud Gateway并开启基于服务发现的路由规则

---
+++ app-spring-cloud-gateway/src/main/java/cn/waynechu/springcloud/gateway/GatewayStartMain.java
@@ -0,0 +1,12 @@
+package cn.waynechu.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayStartMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStartMain.class, args);
+    }
+}
