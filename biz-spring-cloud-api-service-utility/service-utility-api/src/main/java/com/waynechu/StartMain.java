faf6d60a8da6132e542f7543cc52cbe1825849e6
Z-Beatles <waynechu@waynechu.cn>
2019-08-08 14:45:52 +0800

    feat(biz-spring-cloud-api-service-utility): 添加utility公共服务

---
+++ biz-spring-cloud-api-service-utility/service-utility-api/src/main/java/com/waynechu/StartMain.java
@@ -0,0 +1,26 @@
+package com.waynechu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author zhuwei
+ * @date 2019/4/2 10:15
+ */
+@EnableScheduling
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.waynechu"}, exclude = DataSourceAutoConfiguration.class)
+public class StartMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartMain.class, args);
+    }
+
+}
