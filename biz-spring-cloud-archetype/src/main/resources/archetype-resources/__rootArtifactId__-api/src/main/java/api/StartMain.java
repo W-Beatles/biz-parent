e938864f4e0be35e268d3c95dfdfb5a733d10d08
Z-Beatles <waynechu@waynechu.cn>
2020-04-26 17:36:43 +0800

    perf(biz-spring-cloud-archetype): archetype基础项目骨架优化

--- biz-spring-cloud-archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/StartMain.java
+++ biz-spring-cloud-archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/StartMain.java
@@ -1,20 +1,25 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package com.waynechu;
+
+package ${package}.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author zhuwei
  * @date 2019/4/2 10:15
  */
+@EnableAsync
 @EnableScheduling
+@EnableHystrix
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableDiscoveryClient
